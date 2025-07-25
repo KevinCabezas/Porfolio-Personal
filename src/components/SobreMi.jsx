5be77f9de7b374b7c90ad1d1cd9e05a844bdcaeb
KevinCabezas <angelescabesuchi@gmail.com>
2025-07-25 17:17:42 -0300

    Seccion SobreMi craeda

---
+++ src/components/SobreMi.jsx
@@ -0,0 +1,11 @@
+export default function SobreMi() {
+  return (
+    <section
+      id="sobreMi"
+      className="min-h-screen bg-cyan-200 flex flex-col items-center justify-center px-4 py-20"
+    >
+      <h2 className="text-4xl font-bold text-black mb-6">SOBRE MI</h2>
+      <p className="text-black max-w-2xl text-center">Aquí tus lenguajes, tecnologías y herramientas.</p>
+    </section>
+  );
+}
